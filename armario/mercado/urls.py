fe75ee8a980d69cda7a7d392059602c4adc7fde6
Mario Cervantes <marioyeahs@gmail.com>
2021-07-13 20:08:51 -0500

    url's de vistas aniadidas

--- armario/mercado/urls.py
+++ armario/mercado/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name='mercado'
 urlpatterns = [
     path('',views.index, name='index'),
+    path('<int:producto_id>/detalles/', views.detalles, name='detalles'),
+    path('<int:producto_id>/compra/', views.compra, name='compra'),
+    path('<int:producto_id>/venta/', views.venta, name='venta'),
+
 ]
