3d1c3b08958c0bcd5ff8f67512c445c56c99a66a
Mario Cervantes <marioyeahs@gmail.com>
2021-07-16 23:10:49 -0500

    Tipos de tallas para la mercancia

--- armario/mercado/models.py
+++ armario/mercado/models.py
@@ -11,17 +11,35 @@ class Marca(models.Model):
     nombre = models.CharField(max_length=255)
     def __str__(self):
         return self.nombre
+    
 
 class Mercancia(models.Model):
     modelo = models.CharField(max_length=255)
     marca = models.ForeignKey(Marca, on_delete=models.CASCADE, related_name="brand")
+    SIZE_TYPE = (
+        ('M','Men'), 
+        ('W','Women'),
+        ('GS','Grade School'),
+        ('PS','Preschool'),
+        ('IF','Infant'),
+        ('TD','Toddler')
+    )
+    DEPTO =(
+        ('CZ','Calzado'),
+        ('RP','Ropa'),
+        ('JG','Juguete'),
+        ('CL','Coleccionable')
+    )
+    size_type = models.CharField(verbose_name="Miembro",max_length=2, choices=SIZE_TYPE)
+    depto = models.CharField(verbose_name="Departamento",max_length=2, choices=DEPTO)
     def __str__(self):
-        return f"{self.modelo} de {self.marca}"
+        return f"{self.marca} {self.modelo}, {self.size_type}."
 
 
 class Oferta(models.Model):
     monto = models.IntegerField()
     comprador = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    talla = models.CharField(max_length=5, null=True,blank=True)
     articulo = models.ForeignKey(Mercancia, on_delete=models.CASCADE)
     def __str__(self):
         return f"${self.monto}.00 mxn en {self.articulo}"
