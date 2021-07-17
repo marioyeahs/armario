f55e629d0d1ee35312bc351596d8848f173baea9
Mario Cervantes <marioyeahs@gmail.com>
2021-07-16 23:11:48 -0500

    Oferta agregada en BDD correctamente

--- armario/mercado/urls.py
+++ armario/mercado/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('<int:producto_id>/detalles/', views.detalles, name='detalles'),
     path('<int:producto_id>/compra/', views.compra, name='compra'),
     path('<int:producto_id>/venta/', views.venta, name='venta'),
+    path('<int:producto_id>/<str:talla>/comprado/<int:total>', views.comprado,name='comprado'),
 
 ]
--- armario/mercado/views.py
+++ armario/mercado/views.py
@@ -5,6 +5,24 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 from .models import Cliente, Marca, Mercancia, Oferta
 
+def fam_member(type,dept):
+    sizes = []
+    if dept == 'CZ':
+        if type == 'M':
+            for i in range(23,31):
+                sizes+=[f'{i} cm']
+        elif type == 'W':
+            for i in range (23,28):
+                sizes+=[f'{i} cm W']
+        elif type == 'GS':
+            for i in range (21,25):
+                sizes+=[f'{i} cm ']
+        elif type == 'PS':
+            for i in range (15,21):
+                sizes+=[f'{i} cm ']
+    elif dept == 'RP':
+        sizes=['XS','S','M','L','XL','XXL']
+    return sizes
 
 def index(request):
     productos = Mercancia.objects.all()
@@ -12,13 +30,48 @@ def index(request):
 
 def detalles(request,producto_id):
     producto = get_object_or_404(Mercancia, pk=producto_id)
-    return render(request,"mercado/detail.html", {'producto':producto})
-
+    tallas = fam_member(producto.size_type,producto.depto)
+    return render(request,"mercado/detalles.html", {
+        'producto':producto,
+        'tallas':tallas,
+        })
 
 def compra(request, producto_id):
-    return HttpResponse('Comprar producto: %s' % producto_id)
+    producto = get_object_or_404(Mercancia, pk=producto_id)
+    try:
+        talla=request.POST['talla']
+        #comprador
+        monto=int(request.POST['monto'])
+        total=monto+200
+    except(KeyError,producto.DoesNotExist):
+        return render(request, "mercado/compra.html",{
+            'producto':producto,
+            'error_message':"You didn´t select a Size"
+            })
+    else:
+        return render(request,"mercado/compra.html", {
+            'producto':producto,
+            'total':total,
+            'talla':talla,
+            'monto':monto,
+            })
 
 def venta(request,producto_id):
     producto = get_object_or_404(Mercancia, pk=producto_id)
     return render(request,"mercado/venta.html", {'producto':producto})
+
+def comprado(request,producto_id,talla,total):
+    p = Cliente.objects.get(pk=5)
+    producto = get_object_or_404(Mercancia, pk=producto_id)
+    # talla = request.POST['talla']
+    # total = request.POST['total']
+    o=Oferta(monto=total, comprador=p,talla=talla,articulo=producto)
+    o.save()
+    
+    return render(request,"mercado/comprado.html",{
+        "producto":producto,
+        "talla":talla,
+        "total":total,
+        })
+
 
