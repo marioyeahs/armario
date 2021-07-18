8363ad8e1a96521e09ccd3912f2e48acaf762aa2
Mario Cervantes <marioyeahs@gmail.com>
2021-07-18 18:16:10 -0500

    urls y templates en settings para user.auth de Django

--- armario/armario/settings.py
+++ armario/armario/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'armario.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['./templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,3 +128,11 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = '/'
+
+#Email support  in console
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
