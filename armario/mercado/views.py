e527b889573f57ff8db3eb193172f28394ab2c75
Mario Cervantes <marioyeahs@gmail.com>
2021-07-22 19:56:19 -0500

    mostrar tallas con la ultima oferta realizada respectivamente

--- armario/mercado/views.py
+++ armario/mercado/views.py
@@ -32,11 +32,16 @@ def index(request):
 def detalles(request,producto_id):
     producto = get_object_or_404(Mercancia, pk=producto_id)
     tallas = fam_member(producto.size_type,producto.depto)
-    ofertas = Oferta.objects.filter(articulo=producto_id)
+    #ofertas = Oferta.objects.filter(articulo=producto_id)
+    ofertas=[]
+    for i in tallas:
+        ofertas+=[Oferta.objects.filter(articulo=producto,talla=i).last()]
+    ventas=zip(tallas,ofertas)
     return render(request,"mercado/detalles.html", {
         'producto':producto,
         'tallas':tallas,
         'ofertas':ofertas,
+        'ventas':ventas,
         })
 
 def compra(request, producto_id):
