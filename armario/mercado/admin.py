f6502e31026e9803c640a765ee41f7ff24da4438
Mario Cervantes <marioyeahs@gmail.com>
2021-07-28 17:29:30 -0500

    modelo para ofertas de venta en admin

--- armario/mercado/admin.py
+++ armario/mercado/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Oferta_compra, Mercancia, Marca, Cliente
+from .models import Oferta_compra, Mercancia, Marca, Cliente, Oferta_venta
 
 admin.site.register(Oferta_compra)
+admin.site.register(Oferta_venta)
 admin.site.register(Marca)
 admin.site.register(Cliente)
 admin.site.register(Mercancia)
