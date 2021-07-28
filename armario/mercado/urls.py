eb10768584bca88c1e48ad0137694fbaadf769dc
Mario Cervantes <marioyeahs@gmail.com>
2021-07-28 17:30:08 -0500

    url's para venta

--- armario/mercado/urls.py
+++ armario/mercado/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('<int:producto_id>/compra/', views.compra, name='compra'),
     path('<int:producto_id>/venta/', views.venta, name='venta'),
     path('<int:producto_id>/comprado/', views.comprado,name='comprado'),
+    path('<int:producto_id>/vendido/', views.vendido,name='vendido'),
     path('registro/', views.register,name='register')
 
 ]
