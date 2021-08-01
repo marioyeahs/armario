c5c18f4c8a9e9f4532160ee86a707e30580cf04d
Mario Cervantes <marioyeahs@gmail.com>
2021-08-01 17:33:28 -0500

    se quitó url de Mercancia DetailView

--- armario/mercado/urls.py
+++ armario/mercado/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from . import views
-from mercado.views import MercanciaListView, MercanciaDetailView, MarcaListView
+from mercado.views import MercanciaListView, MarcaListView
 
 app_name='mercado'
 urlpatterns = [
@@ -14,7 +14,7 @@ urlpatterns = [
     path('<int:producto_id>/vendido', views.vendido,name='vendido'),
     path('registro/', views.register,name='register'),
     path('mis_ofertas/', views.mis_ofertas,name='mis_ofertas'),
-    path('mercancia/<int:pk>', MercanciaDetailView.as_view(),name='mercancia-detail'),
+    #path('mercancia/<int:pk>', MercanciaDetailView.as_view(),name='mercancia-detail'),
     path('mercancia/<marca>', MarcaListView.as_view(), name='marca')
 
     #path('myurl/<int:fish>', views.my_view, {'my_template_name': 'some_path'}, name='aurl'),
