08c70b87573514a1b4c52504b0268edb42bdf72d
Mario Cervantes <marioyeahs@gmail.com>
2021-08-01 00:25:32 -0500

    urls for list products by brand

--- armario/mercado/urls.py
+++ armario/mercado/urls.py
@@ -1,18 +1,22 @@
 from django.urls import path
 
 from . import views
-from mercado.views import MercanciaListView
+from mercado.views import MercanciaListView, MercanciaDetailView, MarcaListView
 
 app_name='mercado'
 urlpatterns = [
-    path('',views.index, name='index'),
-    path('<int:producto_id>/detalles/', views.detalles, name='detalles'),
-    path('<int:producto_id>/compra/', views.compra, name='compra'),
-    path('<int:producto_id>/venta/', views.venta, name='venta'),
-    path('<int:producto_id>/comprado/', views.comprado,name='comprado'),
-    path('<int:producto_id>/vendido/', views.vendido,name='vendido'),
+    #path('',views.index, name='index'),
+    path('', MercanciaListView.as_view(),name='index'),
+    path('<int:producto_id>/detalles', views.detalles, name='detalles'),
+    path('<int:producto_id>/compra', views.compra, name='compra'),
+    path('<int:producto_id>/venta', views.venta, name='venta'),
+    path('<int:producto_id>/comprado', views.comprado,name='comprado'),
+    path('<int:producto_id>/vendido', views.vendido,name='vendido'),
     path('registro/', views.register,name='register'),
     path('mis_ofertas/', views.mis_ofertas,name='mis_ofertas'),
-    path('mercancia/', MercanciaListView.as_view()),
+    path('mercancia/<int:pk>', MercanciaDetailView.as_view(),name='mercancia-detail'),
+    path('mercancia/<marca>', MarcaListView.as_view(), name='marca')
+
+    #path('myurl/<int:fish>', views.my_view, {'my_template_name': 'some_path'}, name='aurl'),
 
 ]
