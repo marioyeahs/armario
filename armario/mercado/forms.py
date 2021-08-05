7fa43e5484cfd1f89a6b9a9542550d3ee4b91b3f
Mario Cervantes <marioyeahs@gmail.com>
2021-08-04 23:41:59 -0500

    nombre del form corregido

--- armario/mercado/forms.py
+++ armario/mercado/forms.py
@@ -3,7 +3,7 @@ from django import forms
 class sizeForm(forms.Form):
     monto = forms.IntegerField()
 
-class registerForm(forms.Form):
+class RegisterForm(forms.Form):
     usuario = forms.CharField(max_length=250)
     phone = forms.IntegerField()
     email = forms.EmailField(label='E-mail')
