edf2361649c675d30a04a0be0debf41ed772b73c
Mario Cervantes <marioyeahs@gmail.com>
2021-08-04 23:46:41 -0500

    se agregó form de compra

--- armario/mercado/forms.py
+++ armario/mercado/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-class sizeForm(forms.Form):
+class SizeForm(forms.Form):
     monto = forms.IntegerField()
 
 class RegisterForm(forms.Form):
@@ -8,3 +8,7 @@ class RegisterForm(forms.Form):
     phone = forms.IntegerField()
     email = forms.EmailField(label='E-mail')
     passwd = forms.CharField(max_length=250, widget=forms.PasswordInput)
+
+class BuyingForm(forms.Form):
+    talla = forms.CharField(max_length=5)
+    total = forms.IntegerField()
