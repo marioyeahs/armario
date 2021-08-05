944ee87000cec01cbb18f1a13830ee255f39ed03
Mario Cervantes <marioyeahs@gmail.com>
2021-08-05 01:10:31 -0500

    url de form para registro en vista basada en clase

--- armario/mercado/urls.py
+++ armario/mercado/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from . import views
-from mercado.views import IndexListView, MarcaListView
+from mercado.views import IndexListView, MarcaListView, RegisterFormView
 
 app_name='mercado'
 urlpatterns = [
@@ -12,14 +13,12 @@ urlpatterns = [
     path('<int:producto_id>/venta', views.venta, name='venta'),
     path('<int:producto_id>/comprado', views.comprado,name='comprado'),
     path('<int:producto_id>/vendido', views.vendido,name='vendido'),
-    path('registro/', views.register,name='register'),
+    path('registro/', RegisterFormView.as_view(),name='register'),
     path('mis_ofertas/', views.mis_ofertas,name='mis_ofertas'),
     #path('mercancia/<int:pk>', MercanciaDetailView.as_view(),name='mercancia-detail'),
     path('mercancia/<marca>', MarcaListView.as_view(), name='marca'),
     path('mis_ofertas/<int:oferta_id>/eliminar_venta', views.eliminar_venta, name='eliminar_venta'),
     path('mis_ofertas/<int:oferta_id>/eliminar_compra', views.eliminar_compra, name='eliminar_compra'),
-    
-
     #path('myurl/<int:fish>', views.my_view, {'my_template_name': 'some_path'}, name='aurl'),
 
 ]
