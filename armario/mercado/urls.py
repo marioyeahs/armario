5ba43c688dcafb489870b308cf1b9b69ea0f1033
Mario Cervantes <marioyeahs@gmail.com>
2021-08-16 18:22:28 -0500

    url's de venta y compra con oferta actual

--- armario/mercado/urls.py
+++ armario/mercado/urls.py
@@ -20,6 +20,9 @@ urlpatterns = [
     path('mis_ofertas/<int:oferta_id>/eliminar_venta', views.eliminar_venta, name='eliminar_venta'),
     path('mis_ofertas/<int:oferta_id>/eliminar_compra', views.eliminar_compra, name='eliminar_compra'),
     #path('myurl/<int:fish>', views.my_view, {'my_template_name': 'some_path'}, name='aurl'),
-    path('mas_vendidos/',MasVendidosListView.as_view(), name='mas_vendidos')
-
+    path('mas_vendidos/',MasVendidosListView.as_view(), name='mas_vendidos'),
+    path('<int:producto_id>/oferta_venta', views.oferta_venta, name='oferta_venta'),
+    path('<int:producto_id>/oferta_vendida', views.oferta_vendida, name='oferta_vendida'),
+    path('<int:producto_id>/oferta_compra', views.oferta_compra, name='oferta_compra'),
+    path('<int:producto_id>/oferta_comprada', views.oferta_comprada, name='oferta_comprada'),
 ]
