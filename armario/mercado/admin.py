ab07c48d4dc288aac633edb1ea79d00119119cea
Mario Cervantes <marioyeahs@gmail.com>
2021-08-16 18:50:46 -0500

    se agregó modelo a admin

--- armario/mercado/admin.py
+++ armario/mercado/admin.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Oferta_compra, Mercancia, Marca, Cliente, Oferta_venta
+from .models import Oferta_compra, Mercancia, Marca, Cliente, Oferta_venta, Ofertas_compradas
 
 admin.site.register(Oferta_compra)
 admin.site.register(Oferta_venta)
 admin.site.register(Marca)
 admin.site.register(Cliente)
 admin.site.register(Mercancia)
+admin.site.register(Ofertas_compradas)
