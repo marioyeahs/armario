9b898d0de73bd020016e8dbdc9b4c357b76b8fbe
Mario Cervantes <marioyeahs@gmail.com>
2021-08-16 18:28:02 -0500

    verbose name de aplicación

--- armario/mercado/apps.py
+++ armario/mercado/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class MercadoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'mercado'
+    verbose_name = 'Mercado Aplication'
