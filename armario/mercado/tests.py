46bfc18f88cc4ba177a9e5d4296bcc1c38df7385
Mario Cervantes <marioyeahs@gmail.com>
2021-08-20 23:19:52 -0500

    add: test for monto greater than

--- armario/mercado/tests.py
+++ armario/mercado/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.urls import reverse
+
 
 # Create your tests here.
+
+from mercado.models import Cliente, Oferta_compra
+
+class Oferta_compraModelTest(TestCase):
+    def test_monto_oferta_mayor_envio(self):
+        """si la oferta es mayor al costo de envío retorna True"""
+        oferta = Oferta_compra(monto=200)
+        self.assertIs(oferta.oferta_mayor_envio(),False)
+    
