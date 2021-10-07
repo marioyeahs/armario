7cf53637ae8ada81f5fb57abdc849d19fa5b665b
Mario Cervantes <marioyeahs@gmail.com>
2021-10-06 23:03:56 -0500

    add: js to fetch own server

--- armario/mercado/static/mercado/js/index.js
+++ armario/mercado/static/mercado/js/index.js
@@ -5,12 +5,22 @@ function show_products(key){
     document.querySelector(`#${key}`).style.display = 'block';
 }
 
+function show_sections(marca) {
+    fetch(`./productos/${marca}/`)
+    .then(response => response.text()
+    )
+    .then(text => {
+        document.querySelector('#content').innerHTML = text;
+    });
+}
+
 document.addEventListener('DOMContentLoaded', function(){
 
     document.querySelectorAll('button').forEach(button => {
         button.onclick = function() {
             document.getElementById('productos').style.display = 'none';
             show_products(this.dataset.key);
+            show_sections(this.dataset.marca);
         }
-    })
+    });
 });
