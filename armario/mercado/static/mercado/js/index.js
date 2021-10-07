b53b84e5ea2401ac4a5879b8ce877cda21b97ab5
Mario Cervantes <marioyeahs@gmail.com>
2021-10-06 23:29:51 -0500

    add: function fetch django view

--- armario/mercado/static/mercado/js/index.js
+++ armario/mercado/static/mercado/js/index.js
@@ -1,16 +1,16 @@
-function show_products(key){
-    document.querySelectorAll('div').forEach(div => {
-        div.style.display = 'none';
+function show_products_by_department(depto){
+    fetch(`./departamento/${depto}`)
+    .then(response => response.text())
+    .then( text => {
+        document.querySelector('#products_by_department').innerHTML = text;
     });
-    document.querySelector(`#${key}`).style.display = 'block';
 }
 
-function show_sections(marca) {
+function show__products_by_brand(marca) {
     fetch(`./productos/${marca}/`)
-    .then(response => response.text()
-    )
+    .then(response => response.text())
     .then(text => {
-        document.querySelector('#content').innerHTML = text;
+        document.querySelector('#products_by_brand').innerHTML = text;
     });
 }
 
@@ -19,8 +19,8 @@ document.addEventListener('DOMContentLoaded', function(){
     document.querySelectorAll('button').forEach(button => {
         button.onclick = function() {
             document.getElementById('productos').style.display = 'none';
-            show_products(this.dataset.key);
-            show_sections(this.dataset.marca);
+            show_products_by_department(this.dataset.depto);
+            //show_products_by_brand(this.dataset.marca);
         }
     });
 });
