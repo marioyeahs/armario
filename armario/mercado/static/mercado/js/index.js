e73996fb4e2db71727ffbc81e3ad8e279870483e
Mario Cervantes <marioyeahs@gmail.com>
2021-10-07 23:13:48 -0500

    add: display property and and check class button

--- armario/mercado/static/mercado/js/index.js
+++ armario/mercado/static/mercado/js/index.js
@@ -2,14 +2,18 @@ function show_products_by_department(depto){
     fetch(`./departamento/${depto}`)
     .then(response => response.text())
     .then( text => {
+        document.getElementById("products_by_brand").style.display="none";
+        document.getElementById("products_by_department").style.display="block";
         document.querySelector('#products_by_department').innerHTML = text;
     });
 }
 
-function show__products_by_brand(marca) {
+function show_products_by_brand(marca) {
     fetch(`./productos/${marca}/`)
     .then(response => response.text())
     .then(text => {
+        document.getElementById("products_by_department").style.display="none";
+        document.getElementById("products_by_brand").style.display="block";
         document.querySelector('#products_by_brand').innerHTML = text;
     });
 }
@@ -18,9 +22,11 @@ document.addEventListener('DOMContentLoaded', function(){
 
     document.querySelectorAll('button').forEach(button => {
         button.onclick = function() {
-            document.getElementById('productos').style.display = 'none';
-            show_products_by_department(this.dataset.depto);
-            //show_products_by_brand(this.dataset.marca);
+            if(this.className=="marca"){
+                show_products_by_brand(this.dataset.marca);
+            }else if(this.className=="depto"){
+                show_products_by_department(this.dataset.depto);
+            }
         }
     });
 });
