0031f66674c2016df32aedae0b8e95e8d41fddea
Mario Cervantes <marioyeahs@gmail.com>
2021-10-20 23:48:12 -0500

    add: debug_toolbar to project

--- armario/armario/urls.py
+++ armario/armario/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+import debug_toolbar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('mercado/', include('mercado.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('__debug__/', include(debug_toolbar.urls)),
 ]
