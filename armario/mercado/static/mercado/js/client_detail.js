049010a7ba17beed3fe9f459a96594332029aa5c
marioyeahs <marioyeahs@gmail.com>
2021-11-15 23:59:32 -0600

    add: js file to fetch offers and bids

--- armario/mercado/static/mercado/js/client_detail.js
+++ armario/mercado/static/mercado/js/client_detail.js
@@ -1,19 +1,26 @@
-function show_my_bids(){
-    fetch(`./pujas`)
+function show(className){
+    fetch(`./${className}`)
     .then(response => response.text())
     .then( text => {
-        document.getElementById("offers").style.display="none";
-        document.getElementById("edit_profile").style.display="none";
-        document.getElementById("purchases").style.display="none";
-        document.getElementById("bids").style.display="block";
-        document.querySelector('#bids').innerHTML = text;
+        document.querySelectorAll('div').display = "none";
+        div = document.getElementById(`${className}`)
+        div.display = "block";
+        div.innerHTML = text;
+
+        // document.getElementById("offers").style.display="none";
+        // document.getElementById("edit_profile").style.display="none";
+        // document.getElementById("purchases").style.display="none";
+        // document.getElementById("bids").style.display="block";
+        // document.querySelector('#bids').innerHTML = text;
     });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelectorAll('button').forEach(button => {
-        button.onclick = () => {
-            show_my_bids();
+        button.onclick = function() {
+            // console.log(this.className);
+            //show_my_bids();
+            show(this.className);
             // if(this.className=="bids"){
             //     // alert("bids");
             //     show_my_bids();
