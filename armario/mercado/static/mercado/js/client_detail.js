ab7d21020a764af8c33cab31a5031f67400cf0fd
marioyeahs <marioyeahs@gmail.com>
2021-11-16 23:26:16 -0600

    fix: show div when button clicked

--- armario/mercado/static/mercado/js/client_detail.js
+++ armario/mercado/static/mercado/js/client_detail.js
@@ -2,16 +2,16 @@ function show(className){
     fetch(`./${className}`)
     .then(response => response.text())
     .then( text => {
-        document.querySelectorAll('div').display = "none";
-        div = document.getElementById(`${className}`)
-        div.display = "block";
-        div.innerHTML = text;
+        // document.querySelectorAll('div').display = "none";
+        // div = document.getElementById(`${className}`)
+        // div.display = "block";
+        // div.innerHTML = text;
 
         // document.getElementById("offers").style.display="none";
         // document.getElementById("edit_profile").style.display="none";
         // document.getElementById("purchases").style.display="none";
         // document.getElementById("bids").style.display="block";
-        // document.querySelector('#bids').innerHTML = text;
+        document.querySelector('#bids').innerHTML = text;
     });
 }
 
