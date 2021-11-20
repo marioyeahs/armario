b842e8b4c633ddcb776677bdf5b7c10f70524b38
marioyeahs <marioyeahs@gmail.com>
2021-11-19 22:11:54 -0600

    fix: show on only 1 div

--- armario/mercado/static/mercado/js/client_detail.js
+++ armario/mercado/static/mercado/js/client_detail.js
@@ -1,16 +1,7 @@
 function show(className){
-    fetch(`./${className}`)
+    fetch(`./${className}s`)
     .then(response => response.text())
     .then( text => {
-        // document.querySelectorAll('div').display = "none";
-        // div = document.getElementById(`${className}`)
-        // div.display = "block";
-        // div.innerHTML = text;
-
-        // document.getElementById("offers").style.display="none";
-        // document.getElementById("edit_profile").style.display="none";
-        // document.getElementById("purchases").style.display="none";
-        // document.getElementById("bids").style.display="block";
         document.querySelector('#bids').innerHTML = text;
     });
 }
@@ -18,13 +9,7 @@ function show(className){
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelectorAll('button').forEach(button => {
         button.onclick = function() {
-            // console.log(this.className);
-            //show_my_bids();
             show(this.className);
-            // if(this.className=="bids"){
-            //     // alert("bids");
-            //     show_my_bids();
-            // }
         }
     });
 })
